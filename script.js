5e666e6551616b5b677bf0f5328c1478acbf8642
andreas24sfj <andreas24sfj@gmail.com>
2024-11-08 14:49:38 +0100

    oppdatertdiverse

--- script.js
+++ script.js
@@ -12,10 +12,13 @@ async function getData() {
     fetch(url).then((response) => response.json())
   );
 
+
   const pokeDetails = await Promise.all(pokemonPromises);
 
   // console.log(pokemonUrls)
   console.log(pokeDetails);
+  console.log(pokeDetails.map((pokemon)=>pokemon.types.map((allTypes)=>allTypes.type.name)));
+  
 
   content.innerHTML = (pokeDetails.map((pokemon) => {
       return (
@@ -24,10 +27,15 @@ async function getData() {
         <h2>${pokemon.name}</h2>
         <img src="${pokemon.sprites.front_default}"/>
         <img src="${pokemon.sprites.back_default}"/>
+        <p class="stats"><b>type:</b>${pokemon.types.map((allTypes) => `${allTypes.type.name}`).join(` + `)}</p>
+        
+         <p class="stats"><b>weight:</b> ${pokemon.weight}</p>
+         <p class="stats"><b>height:</b> ${pokemon.height}</p>
         </div>
         
         `);
     }))
     .join("<hr>");
 }
-getData();
+ console.log()
+ getData(); 
